4186de083bbe28644ae6e60365668523416abde2
Bakhtovar <bakhtovar.github@gmail.com>
2023-07-07 11:06:54 +0500

    detail ui

--- MovieBase/Controllers/DetailViewController.swift
+++ MovieBase/Controllers/DetailViewController.swift
@@ -26,82 +26,109 @@ class DetailViewController: UIViewController {
         return label
     }()
     
-    lazy var  releaseDateLabel: UILabel = {
+    lazy var releaseDateLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 14)
         label.textColor = .white
-        label.text = "Release Date"
         return label
     }()
-        lazy var averageRatingLabel: UILabel = {
-            let label = UILabel()
-            label.font = UIFont.systemFont(ofSize: 14)
-            label.textColor = .white
-            label.text = "Average Rating: 4.5" // Replace with the actual average rating
-            return label
-        }()
-        
-        lazy var descriptionLabel: UILabel = {
-            let label = UILabel()
-            label.font = UIFont.systemFont(ofSize: 16)
-            label.textColor = .white
-            label.numberOfLines = 0
-            label.text = "Movie description goes here" // Replace with the actual movie description
-            return label
-        }()
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            
-            view.addSubview(backgroundImageView)
-            backgroundImageView.snp.makeConstraints { make in
-                make.edges.equalToSuperview()
-            }
-            
-            backgroundImageView.addSubview(movieImageView)
-            movieImageView.snp.makeConstraints { make in
-                make.leading.equalToSuperview().offset(20)
-                make.top.equalToSuperview().offset(150)
-                make.width.equalTo(100)
-                make.height.equalTo(170)
-            }
-            
-            backgroundImageView.addSubview(titleLabel)
-            titleLabel.snp.makeConstraints { make in
-                make.top.equalToSuperview().offset(20)
-                make.leading.equalToSuperview().offset(20)
-            }
-            
-            backgroundImageView.addSubview(releaseDateLabel)
-            releaseDateLabel.snp.makeConstraints { make in
-                make.top.equalTo(titleLabel.snp.bottom).offset(8)
-                make.leading.equalTo(titleLabel)
-            }
-            
-            backgroundImageView.addSubview(averageRatingLabel)
-            averageRatingLabel.snp.makeConstraints { make in
-                make.top.equalTo(releaseDateLabel.snp.bottom).offset(4)
-                make.leading.equalTo(titleLabel)
-            }
-            
-            backgroundImageView.addSubview(descriptionLabel)
-            descriptionLabel.snp.makeConstraints { make in
-                make.top.equalTo(movieImageView.snp.bottom).offset(20)
-                make.leading.equalTo(movieImageView)
-                make.trailing.equalToSuperview().inset(20)
-            }
+    
+    lazy var averageRatingLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .white
+        label.text = "Average Rating: 4.5" // Replace with the actual average rating
+        return label
+    }()
+    
+    lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = .white
+        label.numberOfLines = 0
+        label.text = "Movie description goes here" // Replace with the actual movie description
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSubviews()
+        makeConstraints()
+    }
+    
+    //MARK: - Private
+    private func addSubviews() {
+        view.addSubview(backgroundImageView)
+        view.addSubview(movieImageView)
+        view.addSubview(titleLabel)
+        view.addSubview(releaseDateLabel)
+        view.addSubview(averageRatingLabel)
+        view.addSubview(descriptionLabel)
+    }
+    
+    private func makeConstraints() {
+        backgroundImageView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.height.equalTo(250)
         }
         
-        public func configure(with model: Title) {
-            titleLabel.text = model.original_title
-            releaseDateLabel.text = "Release Date: \(String(describing: model.release_date))"
-            averageRatingLabel.text = "Average Rating: \(model.vote_average)"
-            descriptionLabel.text = model.overview
-            
-            print(titleLabel.text)
-            if let posterPath = model.poster_path, let url = URL(string: "https://image.tmdb.org/t/p/w500/\(posterPath)") {
-                movieImageView.sd_setImage(with: url, completed: nil)
-                backgroundImageView.sd_setImage(with: url, completed: nil)
-            }
+        movieImageView.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(20)
+            make.top.equalToSuperview().offset(150)
+            make.width.equalTo(100)
+            make.height.equalTo(170)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(backgroundImageView.snp.bottom).offset(20)
+            make.leading.equalTo(movieImageView.snp.trailing).offset(20)
+        }
+        
+        releaseDateLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(8)
+            make.leading.equalTo(titleLabel)
+        }
+        
+        descriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(movieImageView.snp.bottom).offset(20)
+            make.leading.equalTo(movieImageView)
+            make.trailing.equalToSuperview().inset(20)
         }
     }
+    
+    public func configure(with model: Title) {
+        titleLabel.text = model.original_title
+        descriptionLabel.text = model.overview
+        
+        let formattedDate = formatDate(model.release_date)
+        let rating = String(format: "%.1f", model.vote_average)
+        let combinedText = "\(formattedDate) - \(rating) ⭐️"
+        let attributedText = NSAttributedString(string: combinedText, attributes: [
+            NSAttributedString.Key.foregroundColor: UIColor.gray.withAlphaComponent(0.7)
+        ])
+        releaseDateLabel.attributedText = attributedText
+        
+        if let posterPath = model.poster_path, let url = URL(string: "https://image.tmdb.org/t/p/w1280/\(posterPath)") {
+            backgroundImageView.sd_setImage(with: url, completed: nil)
+        }
+        
+        if let posterPath = model.poster_path, let url = URL(string: "https://image.tmdb.org/t/p/w500/\(posterPath)") {
+            movieImageView.sd_setImage(with: url, completed: nil)
+        }
+    }
+    
+    private func formatDate(_ dateString: String?) -> String {
+        guard let dateString = dateString else {
+            return "Unknown"
+        }
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy.MM.dd"
+        
+        if let date = formatter.date(from: dateString) {
+            formatter.dateFormat = "MMM yyyy"
+            return formatter.string(from: date)
+        }
+        
+        return "Unknown"
+    }}
